122e2e0690923e1edf22a4ec4145ee7135d43fb1
Andrii Hryniv <andrii.hryniv@globallogic.com>
2019-04-06 21:24:46 +0300

    bot mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -69,14 +69,12 @@ class Game extends React.Component {
       player_2: 'player 2',
       allowMove: true,
       winner: null,
+      xIsBot: null,
     };
   }
 
   addTile(i) {
-    if (!this.state.allowMove || this.state.winner) {
-      return
-    }
-    axios.post(
+    return axios.post(
       BACKEND_ENDPOINT + '/api/v1/game/add_tile/',
       {
         x_coordinate: i === 0 ? 0 : i % this.state.x_size,
@@ -98,16 +96,87 @@ class Game extends React.Component {
 
       this.setState({
         squares: squares,
-        xIsNext: !this.state.xIsNext,
         winner: winner,
+        xIsNext: !this.state.xIsNext,
       });
     })
     .catch(error => {
+      console.log(error);
       alert(error.response.status + " " + error.response.statusText);
     });
   }
 
-  startNewGame(gameType) {
+  handleClickAddTile(i) {
+    if (!this.state.allowMove || this.state.winner) {
+      return;
+    }
+
+    if (this.state.gameType === GAME_TYPES.MULTIPLAYER) {
+      this.addTile(i)
+    } else if (this.state.gameType === GAME_TYPES.BOT) {
+
+      if (this.state.xIsBot !== this.state.xIsNext) {
+        this.addTile(i).then(() => {
+          this.getAdviceAndMove();
+        });
+      }
+
+    }
+  }
+
+  getAdviceAndMove() {
+    this.getAdvice()
+      .then(data => {
+        const x = data[0];
+        const y = data[1];
+        return (y * this.state.x_size) + x;
+      }).then(i => {
+        this.addTile(i);
+      })
+  }
+
+  handleClickGetAdvice() {
+    const squares = this.state.squares.slice();
+
+    if (this.state.winner) {
+      return;
+    }
+
+    this.getAdvice()
+      .then(data => {
+        const x = data[0];
+        const y = data[1];
+
+        squares[(y * this.state.x_size) + x] = '.';
+        this.setState({
+          squares: squares,
+        });
+      });
+  }
+
+  getAdvice() {
+    const player = this.state.xIsNext ? this.state.player_1 : this.state.player_2;
+
+    this.setState({
+      allowMove: false
+    });
+
+    return axios.get(BACKEND_ENDPOINT + '/api/v1/game/'+this.state.gameId+'/next_move/'+player)
+      .then(response => {
+        const x = response.data.coordinates[0];
+        const y = response.data.coordinates[1];
+        this.setState({
+          allowMove: true,
+        });
+        return [x, y]
+      })
+      .catch(error => {
+        alert(error.response.status + " " + error.response.statusText);
+        console.log(error);
+      });
+  }
+
+  startNewGame(gameType, bot_player = null) {
     axios.post(
       BACKEND_ENDPOINT + '/api/v1/game/',
       {
@@ -126,39 +195,17 @@ class Game extends React.Component {
         xIsNext: true,
         allowMove: true,
         winner: null,
+        xIsBot: bot_player ? (bot_player === 'X') : null,
       });
+    })
+    .then(() => {
+      if (this.state.xIsBot) {
+        this.getAdviceAndMove();
+      }
     })
     .catch(error => {
       console.log(error.response);
       alert(error.response.status + " " + error.response.statusText);
-    });
-  }
-
-  getAdvice() {
-    const player = this.state.xIsNext ? this.state.player_1 : this.state.player_2;
-    const squares = this.state.squares.slice();
-
-    if (this.state.winner) {
-      return;
-    }
-
-    this.setState({
-      allowMove: false
-    });
-    axios.get(BACKEND_ENDPOINT + '/api/v1/game/'+this.state.gameId+'/next_move/'+player)
-    .then(response => {
-      const x = response.data.coordinates[0];
-      const y = response.data.coordinates[1];
-
-      squares[(y * this.state.x_size) + x] = '.';
-      this.setState({
-        squares: squares,
-        allowMove: true,
-      });
-    })
-    .catch(error => {
-      alert(error.response.status + " " + error.response.statusText);
-      console.log(error);
     });
   }
 
@@ -172,12 +219,12 @@ class Game extends React.Component {
     if (this.state.gameId) {
       board = <Board
         squares={squares}
-        onClick={i => this.addTile(i)}
+        onClick={i => this.handleClickAddTile(i)}
         x_size={this.state.x_size}
         y_size={this.state.y_size}
       />;
       if (this.state.gameType === GAME_TYPES.MULTIPLAYER) {
-        advice_button = <button onClick={() => this.getAdvice()}>
+        advice_button = <button onClick={() => this.handleClickGetAdvice()}>
           Get advice
         </button>
       } else {
@@ -199,10 +246,16 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          
+
           <div>
-            <button onClick={() => this.startNewGame(GAME_TYPES.BOT)}>
-              Start new game with bot
+            <button onClick={() => this.startNewGame(GAME_TYPES.BOT, 'O')}>
+              Start new game with bot for X
+            </button>
+          </div>
+
+          <div>
+            <button onClick={() => this.startNewGame(GAME_TYPES.BOT, 'X')}>
+              Start new game with bot for O
             </button>
           </div>
 
@@ -211,6 +264,7 @@ class Game extends React.Component {
               Start new multiplayer game
             </button>
           </div>
+
           <div>
             {advice_button}
           </div>
